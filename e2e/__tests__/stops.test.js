7842da5a682bff5d0fd1990e74a645c4e4d15100
Dylan Corvidae <dylancorvidae@gmail.com>
2019-10-01 16:57:59 -0700

    created stops and tours along with their tests, not finished, still need get all stops and remove a stop that got cancelled 0 attendence.

---
+++ e2e/__tests__/stops.test.js
@@ -0,0 +1,56 @@
+const request = require('../request');
+const db = require('../db');
+
+describe('stops api', () => {
+  beforeEach(() => {
+    return db.dropCollection('stops');
+  });
+
+  const data = {
+    location: {
+      latitude: 45.5266975,
+      longitude: -122.6880503
+    },
+    weather: {
+      any: 'object', //will get from weather api 
+    },
+    attendance: 420
+  };
+  
+  function postStop(stop) {
+    return request
+      .post('/api/stops')
+      .send(stop)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('posts a stop', () => {
+    return postStop(data)
+      .then(stop => {
+        expect(stop).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          location: {
+            latitude: 45.5266975,
+            longitude: -122.6880503
+          },
+          weather: {
+            any: 'object', //will get from weather api 
+          },
+          attendance: 420
+        });
+      });
+  });
+
+  it('gets a stop by id', () => {
+    return postStop(data)
+      .then(stop => {
+        return request.get(`/api/stops/${stop._id}`)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body).toEqual(stop);
+          });
+      });
+  });
+});
--- e2e/__tests__/tours.test.js
+++ e2e/__tests__/tours.test.js
@@ -1,1 +1,53 @@
-const request = require
+const request = require('../request');
+const db = require('../db');
+//const mongoose = require('mongoose');
+//const Stop = require('../../lib/models/stop');
+
+describe('tours api', () => {
+  beforeEach(() => {
+    return db.dropCollection('tours');
+  });
+
+  const data = {
+    title: 'Don\'t Stop Til You Drop!',
+    activities: ['live music', 'food', 'camping', 'butts'],
+    launchDate: new Date(),
+    stops: []
+  };
+
+  function postTour(tour) {
+    return request
+      .post('/api/tours')
+      .send(tour)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+  
+  it('posts a tour', () => {
+    return postTour(data)
+      .then(tour => {
+        expect(tour).toEqual({
+          _id: expect.any(String),
+          __v: 0,
+          title: expect.any(String),
+          activities: ['live music', 'food', 'camping', 'butts'],
+          launchDate: expect.any(String),
+          stops: []
+        });
+      });
+  });
+
+  it('gets a tour by id', () => {
+    return postTour(data)
+      .then(tour => {
+        return request.get(`/api/tours/${tour._id}`)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body).toEqual(tour);
+          });
+      });
+  });
+});
+
+
+
